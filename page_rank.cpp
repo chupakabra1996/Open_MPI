d3b07f896a0b11421905fc322980848618976207
Ramil Safin <RamilSafNab1996@gmail.com>
2017-03-15 22:25:58 +0300

    First edition: all in one, separate multiply

--- page_rank.cpp
+++ page_rank.cpp
@@ -8,7 +8,9 @@
 
 int read_matrix(const char *file, float *&matrix);
 
-void print_vector(const float *vector, int size);
+void print_vector(const float *vector, const int n);
+
+void print_matrix(const float *matrix, const int n);
 
 
 float multiply_vectors(const float *vec1, const float *vec2, const int n) {
@@ -45,104 +47,86 @@ void normalize_pageranks(float *&pageranks, int n) {
 }
 
 
-void iterate_slave(float *pageranks, int n, int chunk_size, float *sub_pageranks, float *sub_matrix) {
-
-    MPI_Bcast(pageranks, n, MPI_FLOAT, 0, MPI_COMM_WORLD);
-
-    multiply(sub_matrix, pageranks, n, chunk_size, sub_pageranks);
-
-    //NOTE: only root process should have valid buffer
-    MPI_Gather(sub_pageranks, chunk_size, MPI_FLOAT, pageranks, chunk_size, MPI_FLOAT, 0, MPI_COMM_WORLD);
-}
-
-
-void iterate_master(float *pageranks, int n, int chunk_size, float *sub_matrix, float *sub_pageranks) {
-
-    MPI_Bcast(pageranks, n, MPI_FLOAT, 0, MPI_COMM_WORLD);
-
-    multiply(sub_matrix, pageranks, n, chunk_size, sub_pageranks);
-
-    MPI_Gather(sub_pageranks, chunk_size, MPI_FLOAT, pageranks, chunk_size, MPI_FLOAT, 0, MPI_COMM_WORLD);
-
-    normalize_pageranks(pageranks, n);
-}
-
-
-void evaluate_slave(int world_size, int iterations) {
-
-    int n; //size of matrix & vector
-
-    MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD); //get the matrix size
-
-    int chunk_size = n / world_size;
-
-    float *sub_matrix = new float[chunk_size * n];
-    float *sub_pageranks = new float[chunk_size];
-    float *pageranks = new float[n];
-
-    MPI_Scatter(sub_matrix, chunk_size * n, MPI_FLOAT, sub_matrix, chunk_size * n, MPI_FLOAT, 0, MPI_COMM_WORLD);
-
-    for (int i = 0; i < iterations; ++i) {
-        iterate_slave(pageranks, n, chunk_size, sub_pageranks, sub_matrix);
-    }
-
-    //clean up
-    delete[] sub_matrix;
-    delete[] pageranks;
-    delete[] sub_pageranks;
-}
-
-
-void evaluate_master(const char *matrix_file, const int world_size, const int iterations) {
-
-    int n; //size of matrix & vector
-    float *matrix;
-
-    n = read_matrix(matrix_file, matrix); //read from file
-
-    int chunk_size = n / world_size;
-
-    float *pageranks = new float[n];
-    float *sub_matrix = new float[chunk_size * n];
-    float *sub_pageranks = new float[chunk_size];
-
-    init_pageranks(n, pageranks); //init it with '1.f/n'
-
-    MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
-    MPI_Scatter(matrix, chunk_size * n, MPI_FLOAT, sub_matrix, chunk_size * n, MPI_FLOAT, 0, MPI_COMM_WORLD);
-
-    for (int i = 0; i < iterations; ++i) {
-        iterate_master(pageranks, n, chunk_size, sub_matrix, sub_pageranks);
-    }
-    print_vector(pageranks, n);
-
-    //clean up
-    delete[] matrix;
-    delete[] sub_matrix;
-    delete[] pageranks;
-    delete[] sub_pageranks;
-
-}
-
-
 int main(int argc, char **argv) {
 
     const int iterations = 1000;
-    int rank, world_size;
+
+    int rank = 0;
+    int world_size = 0;
+
+    int n = 0;
+    int chunk_size = 0;
+    int left_chunk_size = 0;
+
+    float *matrix = nullptr;
+    float *pageranks = nullptr;
+    float *sub_matrix = nullptr;
+    float *sub_pageranks = nullptr;
 
     MPI_Init(&argc, &argv);
 
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    if (rank == 0) { //master
-
-        evaluate_master(argv[1], world_size, iterations);
-
-    } else { //slaves
-
-        evaluate_slave(world_size, iterations);
+    if (rank == 0) {
+        n = read_matrix(argv[1], matrix);
+        left_chunk_size = n % world_size;
     }
+
+    MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+    chunk_size = n / world_size;
+
+    pageranks = new float[n];
+    sub_matrix = new float[chunk_size * n];
+    sub_pageranks = new float[chunk_size];
+
+    if (rank == 0) {
+        init_pageranks(n, pageranks);
+    }
+
+    if (rank == 0) {
+        MPI_Scatter(matrix, chunk_size * n, MPI_FLOAT, sub_matrix, chunk_size * n, MPI_FLOAT, 0, MPI_COMM_WORLD);
+    } else {
+        MPI_Scatter(sub_matrix, chunk_size * n, MPI_FLOAT, sub_matrix, chunk_size * n, MPI_FLOAT, 0, MPI_COMM_WORLD);
+    }
+
+    for (int i = 0; i < iterations; ++i) {
+        MPI_Bcast(pageranks, n, MPI_FLOAT, 0, MPI_COMM_WORLD);
+
+        multiply(sub_matrix, pageranks, n, chunk_size, sub_pageranks);
+
+        if (rank == 0 && left_chunk_size > 0) {
+
+            float left_pagerank[left_chunk_size];
+
+            for (int chunk = left_chunk_size; chunk > 0; --chunk) {
+
+                left_pagerank[left_chunk_size - chunk] = multiply_vectors(&matrix[n * (n - chunk)], pageranks, n);
+            }
+
+            for (int j = 0; j < left_chunk_size; ++j) {
+                pageranks[n - (left_chunk_size - j)] = left_pagerank[j];
+            }
+        }
+
+        MPI_Gather(sub_pageranks, chunk_size, MPI_FLOAT, pageranks, chunk_size, MPI_FLOAT, 0, MPI_COMM_WORLD);
+
+        if (rank == 0) {
+            normalize_pageranks(pageranks, n);
+        }
+
+    }
+
+    if (rank == 0) {
+        print_vector(pageranks, n);
+    }
+
+    delete[] pageranks;
+    delete[] sub_pageranks;
+    delete[] matrix;
+    delete[] sub_matrix;
+
 
     MPI_Finalize();
 
@@ -150,23 +134,23 @@ int main(int argc, char **argv) {
 }
 
 
-void print_matrix(const float *matrix, const int size) {
+void print_matrix(const float *matrix, const int n) {
 
-    for (int i = 0; i < size; ++i) {
-        for (int j = 0; j < size; ++j) {
-            printf("%8.3f", matrix[i * size + j]);
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            printf("%8.3f", matrix[i * n + j]);
         }
         printf("\n");
     }
 }
 
-void print_vector(const float *vector, int size) {
+void print_vector(const float *vector, const int n) {
 
     printf("( ");
-    for (int i = 0; i < size - 1; ++i) {
-        printf("%4.3f\t", vector[i]);
+    for (int i = 0; i < n - 1; ++i) {
+        printf("%4.6f\t", vector[i]);
     }
-    printf("%0.3f )\n", vector[size - 1]);
+    printf("%0.6f )\n", vector[n - 1]);
 }
 
 
